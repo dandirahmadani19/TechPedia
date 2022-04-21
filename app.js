89adfad565e05332f1fcdd9327237882b0c789cc
Dandi Rahmadani <64727624+dandirahmadani19@users.noreply.github.com>
2022-04-22 06:32:08 +0700

    done

--- app.js
+++ app.js
@@ -1,74 +1,23 @@
 const express = require('express');
-const BuyerController = require('./controller/buyerController');
-const SellerController = require('./controller/sellerController');
 const app = express()
-const session = require('express-session');
+const session = require('express-session')
+const routes = require("./routes/");
 const port = 3000
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false}));
+
 app.use(session({
   secret: 'seller secret',
   resave: false,
   saveUninitialized: false,
   cookie: { 
-    secure: false, 
+    secure: false,
     sameSite: true
   }
 }))
-// route untuk buyer-side
 
-// app.get("/", (req, res) => {
-//   res.send('hello')
-// });
-// app.get("/:idCategory", BuyerController.produkByCategory);
-// app.get("/:idCategory/:id", BuyerController.produkByCategory);
-
-// // route untuk seller-side
-app.get("/seller/login", SellerController.login);
-app.post("/seller/login", SellerController.loginPost);
-
-app.get("/seller/register", SellerController.register);
-app.post("/seller/register", SellerController.registerPost);
-
-app.use(function(req, res, next){
-  if(!req.session.userId){
-    const errors = 'Please login first'
-    res.redirect(`/seller/login?error=${errors}`)
-  }else{
-    next();
-  }
-})
-
-app.use(function(req, res, next){
-  if(req.session.userId && req.session.role !== 'seller'){
-    const errors = 'You have no acccess'
-    res.redirect(`/seller/login?error=${errors}`)
-  }else{
-    next();
-  }
-})
-
-app.get("/seller", SellerController.products);
-app.get("/seller/logout", SellerController.logout)
-
-app.get("/seller/categories", SellerController.categories);
-
-app.get("/seller/orderlist", SellerController.orderList);
-
-app.get("/seller/historytransactions", SellerController.historyTransactions);
-
-app.get("/seller/categories/add", SellerController.formAddCategory);
-app.post("/seller/categories/add", SellerController.addCategory)
-
-app.get("/seller/products/add", SellerController.formAddProduct);
-app.post("/seller/products/add", SellerController.addProduct);
-
-app.get("/seller/orderlist/:transactionId", SellerController.processOrder);
-
-app.get("/seller/products/edit/:id", SellerController.formEditProduct);
-app.post("/seller/products/edit/:id", SellerController.updateProduct);
-app.get("/seller/products/delete/:id", SellerController.deleteProduct);
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
---
+++ routes/sellerRoutes.js
@@ -0,0 +1,50 @@
+const sellerRouter = require('express').Router();
+const SellerController = require('../controller/sellerController');
+
+// // route untuk seller-side
+sellerRouter.get("/login", SellerController.login);
+sellerRouter.post("/login", SellerController.loginPost);
+
+sellerRouter.get("/register", SellerController.register);
+sellerRouter.post("/register", SellerController.registerPost);
+
+sellerRouter.use(function(req, res, next){
+  if(!req.session.userId){
+    const errors = 'Please login first'
+    res.redirect(`/login?error=${errors}`)
+  }else{
+    next();
+  }
+})
+
+sellerRouter.use(function(req, res, next){
+  if(req.session.userId && req.session.role !== 'seller'){
+    const errors = 'You have no acccess'
+    res.redirect(`/login?error=${errors}`)
+  }else{
+    next();
+  }
+})
+
+sellerRouter.get("/", SellerController.products);
+sellerRouter.get("/logout", SellerController.logout)
+
+sellerRouter.get("/categories", SellerController.categories);
+
+sellerRouter.get("/orderlist", SellerController.orderList);
+
+sellerRouter.get("/historytransactions", SellerController.historyTransactions);
+
+sellerRouter.get("/categories/add", SellerController.formAddCategory);
+sellerRouter.post("/categories/add", SellerController.addCategory)
+
+sellerRouter.get("/products/add", SellerController.formAddProduct);
+sellerRouter.post("/products/add", SellerController.addProduct);
+
+sellerRouter.get("/orderlist/:transactionId", SellerController.processOrder);
+
+sellerRouter.get("/products/edit/:id", SellerController.formEditProduct);
+sellerRouter.post("/products/edit/:id", SellerController.updateProduct);
+sellerRouter.get("/products/delete/:id", SellerController.deleteProduct);
+
+module.exports = sellerRouter;
